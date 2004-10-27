37a30444fc01b63b7d56c6761b7348f7d3a76733
Joakim Johnsson <joakim@idega.com>
2004-10-27 12:57:09 +0000

    Tryggvi: Fixed IndexOutofBounds bug with state restoring

--- src/java/com/idega/webface/WFBlock.java
+++ src/java/com/idega/webface/WFBlock.java
@@ -1,68 +1,70 @@
 /*
- * Created on 5.9.2003 by  tryggvil in project com.project
+ * Created on 5.9.2003 by tryggvil in project com.project
  */
 package com.idega.webface;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
-
+import java.util.List;
+import java.util.Map;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import com.idega.util.RenderUtils;
 
 /**
- * Component with title bar and container area.
- * Copyright (C) idega software 2003
- * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson</a>
+ * Component with title bar and container area. Copyright (C) idega software
+ * 2003
+ * 
+ * @author <a href="mailto:tryggvi@idega.is">Tryggvi Larusson </a>
  * @author Anders Lindman
  * @version 1.0
  */
-public class WFBlock extends WFContainer
-{
-	
-	public static String RENDERER_TYPE="wf_block";
-	private boolean isInitialized=false;
-	
-	private boolean toolbarEmbeddedInTitlebar=true;
+public class WFBlock extends WFContainer {
+
+	public static String RENDERER_TYPE = "wf_block";
+
+	private boolean isInitialized = false;
+
+	private boolean toolbarEmbeddedInTitlebar = true;
+
 	private String mainAreaStyleClass = WFConstants.STYLE_CLASS_MAINAREA;
-	public WFBlock(){
+
+	public WFBlock() {
 		this("untitled");
 	}
-	
-	public WFBlock(String titleBarText){
 
+	public WFBlock(String titleBarText) {
 		setStyleClass(WFConstants.STYLE_CLASS_BOX);
 		setMainAreaStyleClass(WFConstants.STYLE_CLASS_MAINAREA);
-		
 		WFTitlebar titlebar = new WFTitlebar(titleBarText);
 		setTitlebar(titlebar);
 		setDefaultToolbar();
-		WFContainer mainArea = new WFContainer();
-		super.add(mainArea);
+		//WFContainer mainArea = new WFContainer();
+		//super.add(mainArea);
 	}
-	
+
 	/**
-	 * 
+	 *  
 	 */
 	private void setDefaultToolbar() {
 		WFToolbar toolbar = new WFToolbar();
 		this.setToolbar(toolbar);
-		
 		toolbar.addButton(new WFBackButton());
 		toolbar.addButton(new WFForwardButton());
 		toolbar.addButton(new WFHelpButton());
 		toolbar.addButton(new WFCloseButton());
 	}
 
-	
 	/**
-	 * This method is intended to be implemented in subclasses to add components to mainArea
+	 * This method is intended to be implemented in subclasses to add components
+	 * to mainArea
 	 */
-	protected void initializeContent(){
+	protected void initializeContent() {
 		//does nothing by default
 	}
-	
-	
+
 	/**
 	 * @return
 	 */
@@ -79,7 +81,8 @@ public class WFBlock extends WFContainer
 			if (getTitlebar() != null) {
 				toolbar = getTitlebar().getEmbeddedToolbar();
 			}
-		} else {
+		}
+		else {
 			toolbar = (WFToolbar) getFacets().get("toolbar");
 		}
 		return toolbar;
@@ -101,66 +104,64 @@ public class WFBlock extends WFContainer
 				getFacets().remove("toolbar");
 				getTitlebar().setEmbeddedToolbar(toolbar);
 			}
-		} else {
+		}
+		else {
 			if (getTitlebar() != null) {
 				getTitlebar().removeEmbeddedToolbar();
-			}			
+			}
 			getFacets().put("toolbar", toolbar);
 		}
 	}
 
 	/**
-	 * Sets the css style class for the main area in this block. 
+	 * Sets the css style class for the main area in this block.
 	 */
 	public void setMainAreaStyleClass(String mainAreaStyleClass) {
 		this.mainAreaStyleClass = mainAreaStyleClass;
 	}
-	
+
 	/**
-	 * Returns the css style class  for the main area in this block. 
+	 * Returns the css style class for the main area in this block.
 	 */
 	public String getMainAreaStyleClass() {
 		return mainAreaStyleClass;
 	}
-	
+
 	/**
 	 * Adds a child component to this block.
 	 */
 	public void add(UIComponent child) {
-		WFContainer mainArea = (WFContainer) getChildren().get(0);
-		if (mainArea != null) {
-			mainArea.add(child);
+		WFContainer mainArea = null;
+		if(getChildren().size()==0) {
+			mainArea = new WFContainer();
+			getChildren().add(mainArea);
 		}
+		else {
+			mainArea = (WFContainer) getChildren().get(0);
+		}
+		mainArea.add(child);
 	}
 
-	public String getRendererType(){
+	public String getRendererType() {
 		return RENDERER_TYPE;
 	}
-	
+
 	public void encodeBegin(FacesContext context) throws IOException {
-		if(!isInitialized){
+		if (!isInitialized) {
 			this.initializeContent();
-			this.isInitialized=true;
+			this.isInitialized = true;
 		}
 		super.encodeBegin(context);
 	}
-	
+
 	/*
-	public void encodeBegin(FacesContext context) throws IOException {
-		if (!isRendered()) {
-			return;
-		}
-		super.encodeBegin(context);
-		if (mainAreaStyleClass != null) {
-			WFContainer mainArea = (WFContainer) getChildren().get(0);
-			mainArea.setStyleClass(mainAreaStyleClass);
-		}
-		if (!isToolbarEmbeddedInTitlebar()) {
-			renderFacet(context, "toolbar");
-		}
-		renderFacet(context, "titlebar");
-	}*/
-	
+	 * public void encodeBegin(FacesContext context) throws IOException { if
+	 * (!isRendered()) { return; } super.encodeBegin(context); if
+	 * (mainAreaStyleClass != null) { WFContainer mainArea = (WFContainer)
+	 * getChildren().get(0); mainArea.setStyleClass(mainAreaStyleClass); } if
+	 * (!isToolbarEmbeddedInTitlebar()) { renderFacet(context, "toolbar"); }
+	 * renderFacet(context, "titlebar"); }
+	 */
 	/**
 	 * @see javax.faces.component.UIComponent#encodeChildren(javax.faces.context.FacesContext)
 	 */
@@ -174,7 +175,7 @@ public class WFBlock extends WFContainer
 		//	RenderUtils.renderChild(context,child);
 		//}
 	}
-	
+
 	/**
 	 * @see javax.faces.component.UIComponent#encodeEnd(javax.faces.context.FacesContext)
 	 */
@@ -184,7 +185,7 @@ public class WFBlock extends WFContainer
 		}
 		super.encodeEnd(context);
 	}
-	
+
 	/**
 	 * @return Returns the toolbarEmbeddedInTitlebar.
 	 */
@@ -193,7 +194,8 @@ public class WFBlock extends WFContainer
 	}
 
 	/**
-	 * @param toolbarEmbeddedInTitlebar the toolbarEmbeddedInTitlebar to set
+	 * @param toolbarEmbeddedInTitlebar
+	 *            the toolbarEmbeddedInTitlebar to set
 	 */
 	public void setToolbarEmbeddedInTitlebar(boolean toolbarEmbeddedInTitlebar) {
 		WFToolbar toolbar = getToolbar();
@@ -202,7 +204,7 @@ public class WFBlock extends WFContainer
 			setToolbar(toolbar);
 		}
 	}
-	
+
 	/**
 	 * @see javax.faces.component.UIPanel#saveState(javax.faces.context.FacesContext)
 	 */
@@ -213,16 +215,93 @@ public class WFBlock extends WFContainer
 		values[2] = mainAreaStyleClass;
 		values[3] = new Boolean(isInitialized);
 		return values;
+		//return super.saveState(ctx);
 	}
-	
+
 	/**
-	 * @see javax.faces.component.UIPanel#restoreState(javax.faces.context.FacesContext, java.lang.Object)
+	 * @see javax.faces.component.UIPanel#restoreState(javax.faces.context.FacesContext,
+	 *      java.lang.Object)
 	 */
 	public void restoreState(FacesContext ctx, Object state) {
-		Object values[] = (Object[])state;
+		Object values[] = (Object[]) state;
 		super.restoreState(ctx, values[0]);
 		toolbarEmbeddedInTitlebar = ((Boolean) values[1]).booleanValue();
 		mainAreaStyleClass = (String) values[2];
 		isInitialized = ((Boolean) values[3]).booleanValue();
+		//super.restoreState(ctx,state);
 	}
+
+	public void processRestoreState(FacesContext context, Object state) {
+		super.processRestoreState(context,state);
+		/*
+		if (context == null)
+			throw new NullPointerException("context");
+		Object myState = ((Object[]) state)[0];
+		Map facetMap = (Map) ((Object[]) state)[1];
+		List childrenList = (List) ((Object[]) state)[2];
+		for (Iterator it = getFacets().entrySet().iterator(); it.hasNext();) {
+			Map.Entry entry = (Map.Entry) it.next();
+			Object facetState = facetMap.get(entry.getKey());
+			if (facetState != null) {
+				((UIComponent) entry.getValue()).processRestoreState(context, facetState);
+			}
+			else {
+				context.getExternalContext().log("No state found to restore facet " + entry.getKey());
+			}
+		}
+		int childCount = getChildCount();
+		if (childCount > 0) {
+			int idx = 0;
+			for (Iterator it = getChildren().iterator(); it.hasNext();) {
+				UIComponent child = (UIComponent) it.next();
+				Object childState = childrenList.get(idx++);
+				if (childState != null) {
+					child.processRestoreState(context, childState);
+				}
+				else {
+					context.getExternalContext().log("No state found to restore child of component " + getId());
+				}
+			}
+		}
+		restoreState(context, myState);
+		*/
+	}
+	
+	
+    public Object processSaveState(FacesContext context)
+    {
+    	return super.processSaveState(context);
+    	/*
+        if (context == null) throw new NullPointerException("context");
+        if (isTransient()) return null;
+        Map facetMap = null;
+        for (Iterator it = getFacets().entrySet().iterator(); it.hasNext(); )
+        {
+            Map.Entry entry = (Map.Entry)it.next();
+            if (facetMap == null) facetMap = new HashMap();
+            UIComponent component = (UIComponent)entry.getValue();
+            if (!component.isTransient())
+            {
+                facetMap.put(entry.getKey(), component.processSaveState(context));
+            }
+        }
+        List childrenList = null;
+        if (getChildCount() > 0)
+        {
+            for (Iterator it = getChildren().iterator(); it.hasNext(); )
+            {
+                UIComponent child = (UIComponent)it.next();
+                if (!child.isTransient())
+                {
+                    if (childrenList == null) childrenList = new ArrayList(getChildCount());
+                    childrenList.add(child.processSaveState(context));
+                }
+            }
+        }
+        return new Object[] {saveState(context),
+                             facetMap,
+                             childrenList};
+        */
+    }
+
 }
