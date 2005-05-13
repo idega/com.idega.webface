c832a791b124193c9bcd8561940c8b7f8effbb18
Gudmundur Agust Saemundsson <gummi@idega.com>
2005-05-13 11:20:28 +0000

    *** empty log message ***

---
+++ src/java/com/idega/webface/WFMenuItems.java
@@ -0,0 +1,30 @@
+/*
+ * $Id: WFMenuItems.java,v 1.1 2005/05/13 11:20:28 gummi Exp $
+ * Created on 29.4.2005
+ *
+ * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
+ *
+ * This software is the proprietary information of Idega hf.
+ * Use is subject to license terms.
+ */
+package com.idega.webface;
+
+import javax.faces.component.UISelectItems;
+
+
+/**
+ * 
+ *  Last modified: $Date: 2005/05/13 11:20:28 $ by $Author: gummi $
+ * 
+ * @author <a href="mailto:gummi@idega.com">Gudmundur Agust Saemundsson</a>
+ * @version $Revision: 1.1 $
+ */
+public class WFMenuItems extends UISelectItems {
+
+	/**
+	 * 
+	 */
+	public WFMenuItems() {
+		super();
+	}
+}
