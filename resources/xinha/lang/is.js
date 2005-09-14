29bdb9370d690e96a13a96702675137bbb6f66a2
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-09-14 01:32:51 +0000

    Adding icelandic

---
+++ resources/xinha/lang/is.js
@@ -0,0 +1,32 @@
+// LANG: "is", ENCODING: UTF-8
+
+// Icelandic version for htmlArea v3.0 - Alpha Release
+// - translated by Tryggvi Larusson <tryggvi@idega.is>
+
+{
+  "Bold": "Feitletra",
+  "Italic": "Skáletra",
+  "Underline": "Undirstrika",
+  "Strikethrough": "Útstrika",
+  "Subscript": "Lágskrift",
+  "Superscript": "Háskrift",
+  "Justify Left": "Vinstrijafna",
+  "Justify Center": "Miðja",
+  "Justify Right": "Hægrijafna",
+  "Justify Full": "Fulljafna",
+  "Ordered List": "Númeraður listi",
+  "Bulleted List": "Punktalisti",
+  "Decrease Indent": "Minnka inndrátt",
+  "Increase Indent": "Auka inndrátt",
+  "Font Color": "Litur á texta",
+  "Background Color": "Bakgrunnslitur",
+  "Horizontal Rule": "Lárétt lína",
+  "Insert Web Link": "Setja inn hlekk",
+  "Insert/Modify Image": "Setja inn mynd",
+  "Insert Table": "Setja inn töflu",
+  "Toggle HTML Source": "Sýna kóða",
+  "Enlarge Editor": "Stækka glugga",
+  "About this editor": "Um ritilinn",
+  "Help using editor": "Hjálp",
+  "Current style": "Valinn stíll"
+}
