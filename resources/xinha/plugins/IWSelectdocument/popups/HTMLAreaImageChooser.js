b541623ad4cea7e7e95bca6c73023e2f6ea611a9
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2005-09-14 01:24:56 +0000

    Committing in plugin for idegaWeb for selection of Images and Links in xinha

---
+++ resources/xinha/plugins/IWSelectdocument/popups/HTMLAreaImageChooser.js
@@ -0,0 +1,64 @@
+function Set( string )                // return character
+{
+    var character = string;
+	document.getElementById("uploadForm:f_url").value=string;
+}
+
+function Init() {
+  //__dlg_init();
+  setWindowSizeCentered(550,800);
+  var param = window.dialogArguments;
+  if (param) {
+      document.getElementById("uploadForm:f_url").value = param["f_url"];
+      document.getElementById("uploadForm:f_alt").value = param["f_alt"];
+      document.getElementById("uploadForm:f_border").value = param["f_border"];
+      document.getElementById("f_align").value = param["f_align"];
+      document.getElementById("uploadForm:f_vert").value = param["f_vert"];
+      document.getElementById("uploadForm:f_horiz").value = param["f_horiz"];
+      window.ipreview.location.replace(param.f_url);
+  }
+  document.getElementById("uploadForm:f_url").focus();
+};
+
+function onOK() {
+  var required = {
+    "uploadForm:f_url": "You must enter the URL"
+  };
+  for (var i in required) {
+    var el = document.getElementById(i);
+    if (!el.value) {
+      alert(required[i]);
+      el.focus();
+      return false;
+    }
+  }
+  // pass data back to the calling window
+  var fields = ["f_url", "f_alt", "f_align", "f_border",
+                "f_horiz", "f_vert"];
+  var param = new Object();
+  param["f_url"] = document.getElementById("uploadForm:f_url").value;
+  param["f_alt"] = document.getElementById("uploadForm:f_alt").value;
+  param["f_align"] = document.getElementById("f_align").value;
+  param["f_border"] = document.getElementById("uploadForm:f_border").value;
+  param["f_horiz"] = document.getElementById("uploadForm:f_horiz").value;
+  param["f_vert"] = document.getElementById("uploadForm:f_vert").value;
+  __dlg_close(param);
+  return false;
+};
+
+function onCancel() {
+  __dlg_close(null);
+  return false;
+};
+
+function onPreview() {
+  var f_url = document.getElementById("uploadForm:f_url");
+  var url = f_url.value;
+  if (!url) {
+    alert("You have to enter an URL first");
+    f_url.focus();
+    return false;
+  }
+  window.ipreview.location.replace(url);
+  return false;
+};
