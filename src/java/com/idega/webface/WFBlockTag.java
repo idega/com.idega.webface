be6e3d63b0db60db676ea62a345d1f38dfadfbae
Tryggvi Larusson <tryggvi.larusson@gmail.com>
2006-02-28 12:03:16 +0000

    Added property maximizedVertically and fixed bug with title property

--- src/java/com/idega/webface/WFBlockTag.java
+++ src/java/com/idega/webface/WFBlockTag.java
@@ -1,5 +1,5 @@
 /*
- * $Id: WFBlockTag.java,v 1.4 2005/05/17 16:12:12 tryggvil Exp $
+ * $Id: WFBlockTag.java,v 1.5 2006/02/28 12:03:16 tryggvil Exp $
  *
  * Copyright (C) 2004 Idega. All Rights Reserved.
  *
@@ -16,14 +16,15 @@ import javax.faces.webapp.UIComponentTag;
 /**
  * JSP tag for WFBlock
  * <p>
- * Last modified: $Date: 2005/05/17 16:12:12 $ by $Author: tryggvil $
+ * Last modified: $Date: 2006/02/28 12:03:16 $ by $Author: tryggvil $
  *
  * @author tryggvil
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */
 public class WFBlockTag extends UIComponentTag {
 	
 	private String title;
+	private String maximizedVertically;
 	
 	/**
 	 * @see javax.faces.webapp.UIComponentTag#getRendererType()
@@ -43,6 +44,22 @@ public class WFBlockTag extends UIComponentTag {
 		this.title = title;
 	}
 	
+	/**
+	 * @return Returns the maximizedVertically.
+	 */
+	public String getMaximizedVertically() {
+		return maximizedVertically;
+	}
+
+	
+	/**
+	 * @param maximizedVertically The maximizedVertically to set.
+	 */
+	public void setMaximizedVertically(String maximizedVertically) {
+		this.maximizedVertically = maximizedVertically;
+	}
+
+	
 	public void release() {      
 		super.release();      
 		title = null ;
@@ -52,12 +69,19 @@ public class WFBlockTag extends UIComponentTag {
 		super.setProperties(component);
 		if (component != null) {
 			if(title!=null){
-				if(isValueReference(title)){
+				/*if(isValueReference(title)){
 					ValueBinding vb = getFacesContext().getApplication().createValueBinding(title);
 					component.setValueBinding("title", vb);
 				} else {
 					component.getAttributes().put("title", title);
-				}
+				}*/
+				WFBlock block = (WFBlock)component;
+				block.setTitle(title);
+			}
+			if(getMaximizedVertically()!=null){
+				WFBlock block = (WFBlock)component;
+				boolean maximized = Boolean.valueOf(getMaximizedVertically()).booleanValue();
+				block.setMaximizedVertically(maximized);
 			}
 		}
 	}
