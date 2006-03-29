1677f6dc83817bff907eed194488cf0178583822
Thorhallur Helgason <laddi@idega.com>
2006-03-29 13:15:53 +0000

    *** empty log message ***

--- src/java/com/idega/webface/renderkit/WFRenderKit.java
+++ src/java/com/idega/webface/renderkit/WFRenderKit.java
@@ -4,12 +4,13 @@
 package com.idega.webface.renderkit;
 
 import javax.faces.render.RenderKit;
+
 import com.idega.faces.renderkit.BaseRenderKit;
 import com.idega.webface.WFBezel;
 import com.idega.webface.WFBlock;
 import com.idega.webface.WFConstants;
 import com.idega.webface.WFContainer;
-import com.idega.webface.WFTabbedPane;
+import com.idega.webface.WFMenu;
 import com.idega.webface.WFTitlebar;
 import com.idega.webface.WFToolbar;
 import com.idega.webface.htmlarea.HTMLArea;
@@ -28,7 +29,7 @@ public class WFRenderKit extends BaseRenderKit {
 		super(oldRenderKit);
 		this.addRenderer(WFConstants.FAMILY_WEBFACE,WFContainer.RENDERER_TYPE,new ContainerRenderer());
 		this.addRenderer(WFConstants.FAMILY_WEBFACE,WFBezel.RENDERER_TYPE,new BezelRenderer());
-		this.addRenderer(WFConstants.FAMILY_WEBFACE,WFTabbedPane.RENDERER_TYPE,new MenuRenderer());
+		this.addRenderer(WFConstants.FAMILY_WEBFACE,WFMenu.RENDERER_TYPE,new MenuRenderer());
 		this.addRenderer(WFConstants.FAMILY_WEBFACE,WFBlock.RENDERER_TYPE,new BlockRenderer());
 		this.addRenderer(WFConstants.FAMILY_WEBFACE,WFTitlebar.RENDERER_TYPE,new TitlebarRenderer());
 		this.addRenderer(WFConstants.FAMILY_WEBFACE,WFToolbar.RENDERER_TYPE,new ToolbarRenderer());
