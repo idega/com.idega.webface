175bdaf1fd4ae135c09a3db91a5a8ea001995586
Justinas Rakita <justinas@idega.com>
2006-11-06 12:49:13 +0000

    *** empty log message ***

--- src/java/com/idega/webface/IWTree.java
+++ src/java/com/idega/webface/IWTree.java
@@ -15,24 +15,32 @@ public class IWTree extends HtmlTree{
 	public TreeModel _model;
 	public TreeState _restoredState = null;
     
-    public TreeModel getDataModel() {
+    public IWTree(TreeNode value, boolean showRootNode) {
+		super();
+		// TODO Auto-generated constructor stub
+		setValue(value);
+		setShowRootNode(showRootNode);
+	}
+
+ 
+	public IWTree() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TreeModel getDataModel() {
         if (_model != null) {
             return _model;
         }
         Object value = getValue();
-        System.out.println("Checking node type");
         if (value != null) {
             if (value instanceof TreeModel) {
-            	System.out.println("TreeModel");
                 _model = (TreeModel) value;
             }
             else if (value instanceof IWTreeNode) {
-            	System.out.println("before IWTreeNode");
                 _model = new TreeModelBase((TreeNode) value);
-                System.out.println("after IWTreeNode");
             }
             else if (value instanceof IWTree) {
-            	System.out.println("IWTree");
                 _model = new TreeModelBase((TreeNode) value);
             }
 //            else if (value instanceof PageTreeNode) {
@@ -40,23 +48,17 @@ public class IWTree extends HtmlTree{
 //                _model = new TreeModelBase((TreeNode) value);
 //            }
             else if (value instanceof TreeNode) {
-            	System.out.println("TreeNode");
                 _model = new TreeModelBase((TreeNode) value);
             }            
             else {
-            	System.out.println("Neither");
-//            	System.out.println("value is "+ value.getClass());
                 throw new IllegalArgumentException("Value must be a TreeModel or TreeNode");
             }
         }
-        else System.out.println("value = null");
+//        else System.out.println("value = null");
 
         if (_restoredState != null){
-        	System.out.println("Setting tree state");
-//        	System.out.println(-restoredState.toString());
             _model.setTreeState(_restoredState); // set the restored state (if there is one) on the model
         }
-        System.out.println("have value");
         return _model;
     }    
 }
