6991fcd10b8bfc8c48e6f8649a77bde80e3d18aa
Thorhallur Helgason <laddi@idega.com>
2007-05-21 06:55:48 +0000

    Declaring new block version.

--- src/java/com/idega/webface/convert/WFTimestampConverter.java
+++ src/java/com/idega/webface/convert/WFTimestampConverter.java
@@ -1,11 +1,9 @@
 /*
- * $Id: WFTimestampConverter.java,v 1.1 2005/01/18 10:05:26 gimmi Exp $
- * Created on 17.1.2005
- *
+ * $Id: WFTimestampConverter.java,v 1.1.2.1 2007/05/21 06:55:48 laddi Exp $ Created on 17.1.2005
+ * 
  * Copyright (C) 2005 Idega Software hf. All Rights Reserved.
- *
- * This software is the proprietary information of Idega hf.
- * Use is subject to license terms.
+ * 
+ * This software is the proprietary information of Idega hf. Use is subject to license terms.
  */
 package com.idega.webface.convert;
 
@@ -17,22 +15,35 @@ import javax.faces.convert.Converter;
 import javax.faces.convert.ConverterException;
 import com.idega.util.IWTimestamp;
 
-
 /**
  * 
- *  Last modified: $Date: 2005/01/18 10:05:26 $ by $Author: gimmi $
+ * Last modified: $Date: 2007/05/21 06:55:48 $ by $Author: laddi $
  * 
  * @author <a href="mailto:gimmi@idega.com">gimmi</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.1.2.1 $
  */
 public class WFTimestampConverter implements Converter {
+
+	private String datePattern;
+
+	public WFTimestampConverter() {
+		this(null);
+	}
+
+	public WFTimestampConverter(String datePattern) {
+		this.datePattern = datePattern;
+	}
 
 	public Object getAsObject(FacesContext context, UIComponent component, String value) throws ConverterException {
 		try {
 			IWTimestamp stamp = new IWTimestamp(value);
 			Locale locale = context.getViewRoot().getLocale();
-			return stamp.getLocaleDateAndTime(locale,IWTimestamp.MEDIUM, IWTimestamp.MEDIUM);
-		} catch (Exception e) {
+			if (datePattern != null) {
+				return stamp.getDateString(datePattern, locale);
+			}
+			return stamp.getLocaleDateAndTime(locale, IWTimestamp.MEDIUM, IWTimestamp.MEDIUM);
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 			return value;
 		}
@@ -42,17 +53,21 @@ public class WFTimestampConverter implements Converter {
 		if (value == null) {
 			return null;
 		}
-		if (value instanceof Long){
-			if ( ((Long) value).longValue() == 0 ) {
+		if (value instanceof Long) {
+			if (((Long) value).longValue() == 0) {
 				return null;
 			}
 			String valueStr = value.toString();
 			value = new Date(Long.parseLong(valueStr.toString()));
-		} else if (!(value instanceof Date)) {
+		}
+		else if (!(value instanceof Date)) {
 			return value.toString();
 		}
-		IWTimestamp stamp = new IWTimestamp((Date)value);
+		IWTimestamp stamp = new IWTimestamp((Date) value);
 		Locale locale = context.getViewRoot().getLocale();
-		return stamp.getLocaleDateAndTime(locale,IWTimestamp.MEDIUM, IWTimestamp.MEDIUM);
+		if (datePattern != null) {
+			return stamp.getDateString(datePattern, locale);
+		}
+		return stamp.getLocaleDateAndTime(locale, IWTimestamp.MEDIUM, IWTimestamp.MEDIUM);
 	}
 }
