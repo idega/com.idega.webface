58d7272aebabda4d162af9a4dcc5c4e9411f1a84
Valdas Žemaitis <valdas@idega.com>
2012-10-24 16:20:10 +0300

    Using generics

--- src/java/com/idega/webface/renderkit/BaseRenderer.java
+++ src/java/com/idega/webface/renderkit/BaseRenderer.java
@@ -11,42 +11,41 @@ package com.idega.webface.renderkit;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
+
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.render.Renderer;
+
 import com.idega.faces.renderkit.CssHelper;
 import com.idega.util.RenderUtils;
 
-
 /**
  * Base class for Renderer. With some helper functions.<br>
- * 
+ *
  * @author <a href="mailto:tryggvil@idega.com">tryggvil</a>
  * @version 1.0
  */
 public class BaseRenderer extends Renderer {
 	private CssHelper cssHelper = new CssHelper();
-	
+
 	/**
 	 * @return Returns the cssHelper.
 	 */
 	public CssHelper getCssHelper() {
 		return this.cssHelper;
 	}
-	/* (non-Javadoc)
-	 * @see javax.faces.render.Renderer#encodeChildren(javax.faces.context.FacesContext, javax.faces.component.UIComponent)
-	 */
+
+	@Override
 	public void encodeChildren(FacesContext context, UIComponent component) throws IOException {
-		//super.encodeChildren(context, component);
-		if(getRendersChildren()){
-			Collection children = component.getChildren();
-			for (Iterator iter = children.iterator(); iter.hasNext();) {
-				UIComponent child = (UIComponent) iter.next();
+		if (getRendersChildren()) {
+			Collection<UIComponent> children = component.getChildren();
+			for (Iterator<UIComponent> iter = children.iterator(); iter.hasNext();) {
+				UIComponent child = iter.next();
 				renderChild(context,child);
 			}
 		}
 	}
-	
+
 	protected void renderChild(FacesContext context,UIComponent child) throws IOException{
 		RenderUtils.renderChild(context,child);
 	}
